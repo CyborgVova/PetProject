784e52276804e5a96a6952ed3a58bee873e21558
CyborgVova <eroxin79@mail.ru>
2024-02-21 00:49:19 +0300

    Added Makefile

--- ShortLink/app.sh
+++ ShortLink/app.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 ./cleandocker.sh
 
@@ -6,12 +6,12 @@ if [[ $1 == "inmemory" ]] || [[ $1 == "" ]]; then
     docker build -t inmemory .
     docker run --rm -d --name inmemory -p 8080:8080 inmemory
     clear
-    echo "Service: inmemory mode is started"
+    echo "Service: inmemory mode ..."
     sleep 10
-elif [ $1 == "database" ]; then
+elif [[ $1 == "database" ]]; then
     docker-compose up -d
     clear
-    echo "Service: database mode is started"
+    echo "Service: database mode ..."
     sleep 10
 else
     echo "Invalid parameter"
