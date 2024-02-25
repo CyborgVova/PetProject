3a45a6ec2ef227a4c7f55463340e7d42ca5fbbba
CyborgVova <eroxin79@mail.ru>
2024-02-25 23:08:55 +0300

    TestMockPostBadGenerate is broken

--- ShortLink/internal/handlers_mock_test.go
+++ ShortLink/internal/handlers_mock_test.go
@@ -10,19 +10,17 @@ import (
 )
 
 func TestMockPostNewEntry(t *testing.T) {
-	ctrl1 := gomock.NewController(t)
-	ctrl2 := gomock.NewController(t)
-	defer ctrl1.Finish()
-	defer ctrl2.Finish()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-	baseHandle := NewMockBaseHandle(ctrl1)
-	randomable := NewMockRandomable(ctrl2)
+	baseHandle := NewMockBaseHandle(ctrl)
+	randomable := NewMockRandomable(ctrl)
 
 	testName := "First"
 	t.Run(testName, func(t *testing.T) {
 		rand10 := "abcdefgjik"
-		pLong := pb.LongLink{LongLink: "longlink"}
 		pShort := pb.ShortLink{ShortLink: rand10}
+		pLong := pb.LongLink{LongLink: "longlink"}
 		data := database.Mapping{Short: pShort.ShortLink, Long: pLong.LongLink}
 		s := Server{Storage: "database", HandleDB: baseHandle, HandleRand: randomable}
 		empty := database.Mapping{Short: "", Long: ""}
@@ -37,34 +35,80 @@ func TestMockPostNewEntry(t *testing.T) {
 			t.Errorf("want: %v, got: %v", data.Long, result)
 		}
 	})
+}
 
-	// test := struct {
-	// 	testName string
-	// 	rand10   string
-	// 	data     database.Mapping
-	// 	s        Server
-	// 	pLong    proto.LongLink
-	// 	pShort   proto.ShortLink
-	// }{
-	// 	"First",
-	// 	"abcdefgjik",
-	// 	database.Mapping{Short: "shortlink", Long: "longlink"},
-	// 	Server{Storage: "database", DB: nil},
-	// 	// proto.LongLink{LongLink: ""},
-	// 	// proto.ShortLink{ShortLink: "Empty request field"},
-	// 	proto.LongLink{LongLink: "longlink"},
-	// 	proto.ShortLink{ShortLink: "shortlink"},
-	// }
+func TestMockPostExistLink(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-	// t.Run(testName, func(t *testing.T) {
-	// d := database.Mapping{Short: "", Long: ""}
-	// baseHandle.EXPECT().Find(&d, "long=?", data.Long).Return()
-	// randomable.EXPECT().String10().Return(rand10)
-	// baseHandle.EXPECT().Find(&data, "short=?", data.Short).Return()
-	// baseHandle.EXPECT().Create(&data).Return()
-	// s.Post(context.Background(), &pLong)
-	// })
-	// if result.ShortLink != test.pShort.ShortLink {
-	// 	t.Errorf("want: %v, got: %v", test.data.Long, result)
-	// }
+	baseHandle := NewMockBaseHandle(ctrl)
+	tests := []struct {
+		testName string
+		rand10   string
+		data     database.Mapping
+		s        Server
+		pShort   pb.ShortLink
+		pLong    pb.LongLink
+	}{
+		{
+			testName: "First",
+			s:        Server{Storage: "database", HandleDB: baseHandle},
+			pShort:   pb.ShortLink{ShortLink: "shortlink1"},
+			pLong:    pb.LongLink{LongLink: "longlink1"},
+		}, {
+			testName: "Second",
+			s:        Server{Storage: "database", HandleDB: baseHandle},
+			pShort:   pb.ShortLink{ShortLink: "shortlink2"},
+			pLong:    pb.LongLink{LongLink: "longlink2"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			baseHandle.EXPECT().Find(&tt.data, "long=?", tt.pLong.LongLink).SetArg(0, database.Mapping{Short: tt.pShort.ShortLink, Long: tt.pLong.LongLink}).Return()
+			result, _ := tt.s.Post(context.Background(), &tt.pLong)
+			if result.ShortLink != tt.pShort.ShortLink {
+				t.Errorf("want: %v, got: %v\n", tt.pShort.ShortLink, result.ShortLink)
+			}
+		})
+	}
+}
+
+func TestMockPostBadGenerate(t *testing.T) {
+	ctrl1 := gomock.NewController(t)
+	ctrl2 := gomock.NewController(t)
+	defer ctrl1.Finish()
+	defer ctrl2.Finish()
+
+	baseHandle := NewMockBaseHandle(ctrl1)
+	randomable := NewMockRandomable(ctrl2)
+
+	rand10 := "rutyeiwoqp"
+	s := Server{
+		Storage:    "database",
+		HandleDB:   baseHandle,
+		HandleRand: randomable,
+	}
+	data := database.Mapping{}
+	pShort := pb.ShortLink{ShortLink: "shortlink"}
+	pLong := pb.LongLink{LongLink: "longlink"}
+
+	baseHandle.EXPECT().Find(&data, "long=?", pLong.LongLink).Return()
+
+	randomable.EXPECT().String10().Return(pShort.ShortLink)
+	baseHandle.EXPECT().Find(&data, "short=?", pShort.ShortLink).Return()
+
+	// randomable.EXPECT().String10().Return(pShort.ShortLink)
+	// baseHandle.EXPECT().Find(&data, "short=?", pShort.ShortLink).Return()
+
+	randomable.EXPECT().String10().Return(rand10)
+	baseHandle.EXPECT().Find(&data, "short=?", rand10).Return()
+
+	// data = database.Mapping{}
+	baseHandle.EXPECT().Create(database.Mapping{pShort.ShortLink, pLong.LongLink}).Return()
+
+	result, _ := s.Post(context.Background(), &pLong)
+	if result.ShortLink != pShort.ShortLink {
+		t.Errorf("want: %v, got: %v\n", pShort.ShortLink, result.ShortLink)
+	}
 }
